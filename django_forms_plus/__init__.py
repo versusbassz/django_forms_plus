e0fa203b4fa3504689f908b2f9e82b88e61cfa53
Vladimir Sklyar <versus.post@gmail.com>
2023-01-23 12:53:05 +0400

    refactor, bc: Rename "django_forms_plus.fields" to "django_forms_plus.widgets"

--- django_forms_plus/__init__.py
+++ django_forms_plus/__init__.py
@@ -1,5 +1,5 @@
 from .types import FormSpec, FormState, FormData, DjangoForm, JsonFormResponse, FormResponseAction
-from .fields import DumbInput, CaptchaInput
+from .widgets import DumbInput, CaptchaInput
 from .spec import get_form_spec
 from .forms import *
 from .form_helper import *
