03f0dc0fd32fced819c24f863526dfd012033ece
Vladimir Sklyar <versus.post@gmail.com>
2024-01-15 13:38:47 +0300

    refactor, bc: Move "dfp_field=True" to DfpInputMixin

--- django_forms_plus/widgets.py
+++ django_forms_plus/widgets.py
@@ -1,11 +1,9 @@
-"""
-dfp_field = True  is experimental, not used yet
-"""
-
 from django import forms
 
 
 class DfpInputMixin:
+    dfp_field = True  # is experimental, not used yet
+
     def __init__(self, css_classes: dict | None = None, *args, **kwargs) -> None:
         self.css_classes = css_classes
         super().__init__(*args, **kwargs)
@@ -20,7 +18,6 @@ class CaptchaInput(DumbInput):
 
 
 class FormattedInput(DfpInputMixin, forms.TextInput):
-    dfp_field = True
     dfp_widget_name = 'TextInput'
 
     def __init__(self, input_format: str, attrs: dict | None = None):
@@ -29,8 +26,6 @@ class FormattedInput(DfpInputMixin, forms.TextInput):
 
 
 class SlugInput(DfpInputMixin, forms.TextInput):
-    dfp_field = True
-
     def __init__(self, prefix: str = '',
                  suggestions: list | None = None,
                  *args, **kwargs):
@@ -40,8 +35,6 @@ class SlugInput(DfpInputMixin, forms.TextInput):
 
 
 class DateInput(DfpInputMixin, forms.DateInput):
-    dfp_field = True
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.attrs['placeholder'] = '__.__.____'
@@ -53,15 +46,12 @@ class CheckboxInput(DfpInputMixin, forms.CheckboxInput):
         label_hint - a text near the input
                      (self.label is used in the text above the input)
     """
-    dfp_field = True
-
     def __init__(self, label_hint=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label_hint = label_hint if label_hint else ''
 
 
 class Select(DfpInputMixin, forms.Select):
-    dfp_field = True
     pass
 
 
@@ -70,8 +60,6 @@ class ClearableFileInput(DfpInputMixin, forms.ClearableFileInput):
     Allows set clear_checkbox_label in the constructor.
     The original parent class uses a static attribute.
     """
-    dfp_field = True
-
     def __init__(self, clear_checkbox_label: str | None = None,
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
