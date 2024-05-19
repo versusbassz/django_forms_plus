3146250043e0dee2b9caef9107506372c14367a0
Vladimir Sklyar <versus.post@gmail.com>
2024-05-19 10:30:57 +0300

    dev: Add favicon.ico to dev_site

--- dev_site/apex/urls.py
+++ dev_site/apex/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -6,4 +7,10 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('success/', views.success, name='success'),
+]
+
+# Favicon
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+urlpatterns += [
+    re_path(r'^favicon\.ico$', favicon_view),
 ]
--- dev_site/mysite/settings.py
+++ dev_site/mysite/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
