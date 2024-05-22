e4f68c95f18dc6d5c2637798c25fa6c09038be94
Vladimir Sklyar <versus.post@gmail.com>
2024-05-22 20:57:09 +0300

    refactor: Move validate_soft_errors() js-helper to a separate file (inc/validation-soft.js)

--- front/src/components/fieldslot.js
+++ front/src/components/fieldslot.js
@@ -3,7 +3,8 @@ import classNames from "classnames";
 
 import { FormContext, fieldspec_to_input, FieldError } from "../parts";
 import { check_cl_state, collect_followed_fields,
-         fieldHasCL, getFieldClGroups } from "../inc/conditional-logic";
+         fieldHasCL, getFieldCLGroups } from "../inc/conditional-logic";
+import { validate_soft_errors } from "../inc/validation-soft";
 
 /**
  * @param {import("../types").FormSpec} spec
@@ -110,26 +111,4 @@ export function FieldSlot({ name }) {
       ) : null}
     </div>
   );
-}
-
-function validate_soft_errors(name, field_spec, value) {
-    const errors = [];
-
-    for (const check of field_spec.soft_validators) {
-      switch (check.type) {
-        case 'required':
-          ! value && errors.push(check.message)
-          break;
-        case 'regexp':
-          const regexp = RegExp(check.value);
-          const is_match_raw = regexp.exec(value) !== null;
-          const is_err = check.inverse ? is_match_raw : ! is_match_raw;
-          is_err && errors.push(check.message);
-          break;
-        default:
-          console.warn(`Unknown validator: name:${check.name} type:${check.type}`);
-          break;
-      }
-    }
-    return errors;
 }
